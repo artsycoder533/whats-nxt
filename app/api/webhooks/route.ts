5dba52dcba70099140fdabe0068135c207ae0dba
Natasha Johnson <njohnson533@gmail.com>
2023-12-30 00:47:11 -0500

    chore: add stripe webook to trigger sanity update

---
+++ app/api/webhooks/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import { updateStockInSanity } from "@/app/util/helpers";
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const secret = process.env.STRIPE_WEBHOOK_SECRET;
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.text();
+    const signature = req.headers.get("stripe-signature");
+    const event = stripe.webhooks.constructEvent(body, signature, secret);
+
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+
+      const lineItems = await stripe.checkout.sessions.listLineItems(session.id);
+    //   console.log('lineItems before mapping -->',lineItems.data)
+      if (!lineItems) {
+        throw new Error(`missing line items, ${event.id}`);
+      }
+      //reset cart?
+      //get line items
+     
+      const formattedLineItems = lineItems.data.map((item: { price: { product: any; }; quantity: any; }) => ({
+        id: item.price.product,
+        quantity: item.quantity
+      }));
+    
+    //   console.log("formatted lineItems-->", formattedLineItems);
+      //update quantity in sanity
+      formattedLineItems.forEach((lineItem: FormattedLineItem) => updateStockInSanity(lineItem));
+    }
+
+    return NextResponse.json({ result: event, ok: true });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        message: "something went wrong",
+        ok: false,
+      },
+      { status: 500 }
+    );
+  }
+}
