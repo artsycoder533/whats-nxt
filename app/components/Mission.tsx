0feb2262b75d9381d5a7e585a301fe406707330d
Natasha Johnson <njohnson533@gmail.com>
2023-12-30 00:21:48 -0500

    chore: add mission component

---
+++ app/components/Mission.tsx
@@ -0,0 +1,46 @@
+import Title from "./Title";
+
+const Mission = () => {
+  return (
+    <section id="mission" className="max-w-[1400px] w-[90vw] mx-auto py-20">
+      <Title text="Our Mission"/>
+      <p className="max-w-[65ch] mx-auto font-light">
+        Welcome to WHATS NXT, where we believe in empowering individuals to
+        shape their future and embrace their full potential. As a trailblazing
+        company, we specialize in crafting transformative impact planners,
+        journals, and workbooks that guide you through a journey of
+        self-discovery, productivity, and holistic well-being.
+        <br/>
+        <br/>
+        At WHATS NXT, we understand that life is a dynamic journey filled with challenges and
+        opportunities. Our meticulously designed impact planners are not just
+        notebooks; they are your companions on the path to personal growth.
+        Tackling crucial topics such as productivity, anxiety, and self-care,
+        our planners provide a roadmap for setting meaningful goals, overcoming
+        obstacles, and cultivating a balanced and fulfilling life.<br/>
+        <br/>
+        <br/>
+        In addition to our innovative products, WHATS NXT is committed to fostering a
+        community of like-minded individuals dedicated to continuous
+        improvement. Our seminars, training sessions, and retreats are curated
+        experiences that go beyond conventional learning. Led by experts in the
+        fields of personal development and well-being, these events offer
+        immersive and transformative experiences, equipping you with practical
+        tools to navigate the complexities of modern life.<br/>
+        <br/>
+        <br/>
+        Why choose WHATS NXT? We believe in a holistic approach to personal development, addressing
+        not only professional goals but also the mental, emotional, and physical
+        aspects of your well-being. Our mission is to inspire and support you on
+        your journey to becoming the best version of yourself.<br/>
+        <br/>
+        <br/>
+        Join us at WHATSNXT and embark on a transformative adventure where each day brings new
+        possibilities and opportunities for growth. Your future is
+        waiting—<span className="text-[#B76E79]">what&apos;s next for you?</span>
+      </p>
+    </section>
+  );
+};
+
+export default Mission;
