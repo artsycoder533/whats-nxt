c3f54530f9db2705f218baf9a43c0c1f977c8d4c
Natasha Johnson <njohnson533@gmail.com>
2023-12-30 15:20:09 -0500

    chore: update success page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client";
+import {useEffect } from "react";
+import { useCart } from "../context/CartContext";
+
 
 const Success = () => {
+  const { resetCart } = useCart();
+
+  useEffect(() => {
+    resetCart();
+  }, []);
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24 text-4xl">Order Completed</div>
-  )
-}
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 text-4xl">
+      <h2>Thank you for your order!  We appreciate your business!</h2>
+    </div>
+  );
+};
 
 export default Success;
