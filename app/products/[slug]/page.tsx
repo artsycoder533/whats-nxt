7b0befb10fc22a98914adb8c69d4d951d342cc48
Natasha Johnson <njohnson533@gmail.com>
2023-12-30 00:37:03 -0500

    chore: add dynamic product page

---
+++ app/products/[slug]/page.tsx
@@ -0,0 +1,50 @@
+import ProductCard from "@/app/components/ProductCard";
+import { client } from "@/app/lib/sanity";
+import Link from "next/link";
+import { HiArrowLeft } from "react-icons/hi2";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export const revalidate = 30;
+
+async function getProductBySlug(slug: string) {
+  const query = `
+  *[_type == 'product' && slug.current == "${slug}"][0]{
+    _id,
+      name,
+      description,
+      color, price,
+      stockQuantity,
+      'category':category->name,
+      'slug':slug.current,
+      'images': images[]{
+        'altText': altText, 
+        'url': image.asset->url
+      },
+      videos[] {
+        "url": videoFile.asset->url,
+        altText
+      }
+    }
+  `;
+
+  const data = await client.fetch(query);
+  return data;
+}
+
+export default async function ProductPage({ params }: Props) {
+  if (!params.slug) return;
+
+  const product = await getProductBySlug(params.slug);
+
+  return (
+    <section className="flex flex-col justify-center gap-8 max-w-[1400px] w-[90vw] mx-auto my-32">
+      <Link href="/products" className="flex items-center gap-2 text-light"><HiArrowLeft className="text-[#B76E79]"/>Back</Link>
+      <ProductCard product={product} />
+    </section>
+  );
+}
