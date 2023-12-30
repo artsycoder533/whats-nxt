c4f925a792bddc687fd6be36a9d26a672ddb927b
Natasha Johnson <njohnson533@gmail.com>
2023-12-30 00:32:33 -0500

    chore: add product component

---
+++ app/components/Product.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+
+type Props = {
+  product: Products;
+};
+
+
+const Product = ({ product }: Props) => {
+  const { images, name, price, color, slug } = product;
+
+  return (
+    <Link href={`/products/${slug}`} className=" max-w-96">
+      <div className="">
+        <Image
+          src={images?.url}
+          alt={images?.altText}
+          className="rounded-xl h-full w-full object-cover object-center"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="mt-4">
+        <div className="flex justify-between">
+          <p className="font-light">{name}</p>
+          <p className="font-medium">${price}</p>
+        </div>
+        <p className="text-gray-600 text-sm font-extralight">{color}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default Product;
