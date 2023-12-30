a6f72d8011d638636ad6b1b482fc99ee0ab2247a
Natasha Johnson <njohnson533@gmail.com>
2023-12-30 16:03:57 -0500

    chore: fix hero styles

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,7 +6,7 @@ import { HiArrowDown, HiArrowRight } from 'react-icons/hi2';
 
 const Hero = () => {
   return (
-    <section className="flex h-[70vh] flex-col justify-center  gap-12 relative">
+    <section className="flex h-[80vh] flex-col justify-center  gap-12 relative">
       {/* Video Background */}
       <Image src={hero} alt="Woman sitting on couch filling out weekly planner page." className="absolute top-0 left-0 w-full h-full object-cover"/>
       {/* <video
@@ -24,19 +24,19 @@ const Hero = () => {
         purchase!
       </p> */}
       {/* Overlay */}
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-70"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-80"></div>
 
-      <div className="z-10 text-white flex flex-col max-w-[1400px] w-[90vw] mx-auto">
-        <h1 className="text-7xl flex flex-col gap-1 font-extralight">
+      <div className="z-10 text-white flex flex-col justify-center max-w-[1400px] w-[90vw] mx-auto mt-12">
+        <h1 className="text-8xl flex flex-col gap-1 font-extralight">
           <span className="flex gap-2">Imagine</span>
-          <HiArrowDown className="text-[#B76E79] my-2"/>
+          <HiArrowDown className="text-[#B76E79] my-2 text-4xl"/>
           <span className="flex gap-2">Plan</span>
-          <HiArrowDown className="text-[#B76E79] my-2"/>
+          <HiArrowDown className="text-[#B76E79] my-2 text-4xl"/>
           <span className="flex gap-2">Execute</span>
         </h1>
         <Link
           href="/products"
-          className="animate-bounce py-3 px-4 bg-[#B76E79] text-white text-center mt-32 self-center rounded-md hover:bg-[#8a525a]"
+          className="animate-bounce py-3 px-4 bg-[#B76E79] text-white text-center mt-20 self-center rounded-md hover:bg-[#8a525a]"
         >
           Shop WHATS NXT
         </Link>
