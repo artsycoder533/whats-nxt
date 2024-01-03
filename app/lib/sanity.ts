83053fdbfb7416ce0b0e107eec92fc22ec7c6c58
Natasha Johnson <njohnson533@gmail.com>
2024-01-03 00:05:03 -0500

    chore: update api version

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -3,7 +3,7 @@ import {createClient} from 'next-sanity'
 export const client = createClient({
     projectId: '9lh62z8s',
     dataset: 'production', 
-    apiVersion: '2023-10-16',
+    apiVersion: '2023-12-31',
     useCdn: true,
     token: process.env.TOKEN,
 });
