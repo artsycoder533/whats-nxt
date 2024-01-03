0093af681d79b5da4eee4cacc569d9e9113b6161
Natasha Johnson <njohnson533@gmail.com>
2024-01-03 00:04:39 -0500

    chore: add metadata for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Hero from "./components/Hero";
 import Mission from "./components/Mission";
 import Contact from "./components/Contact";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Home',
+}
 
 export default async function Home() {
   return (
