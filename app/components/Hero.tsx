43d16c7f9ac7bc12c2dac492ee6f5610533443a0
Natasha Johnson <njohnson533@gmail.com>
2024-01-03 00:30:21 -0500

    chore: adjust hero styling

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,17 +2,17 @@ import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import hero from '../../public/images/hero.gif';
-import { HiArrowDown, HiArrowRight } from 'react-icons/hi2';
+import { HiArrowDown } from 'react-icons/hi2';
 
 const Hero = () => {
   return (
-    <section className="flex h-[80vh] flex-col justify-center  gap-12 relative">
+    <section className="flex h-[80vh] flex-col justify-center gap-12 relative">
       <Image src={hero} alt="Woman sitting on couch filling out weekly planner page." className="absolute top-0 left-0 w-full h-full object-cover" priority/>
 
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-80"></div>
       
-      <div className="z-10 text-white flex flex-col justify-center max-w-[1400px] w-[90vw] mx-auto mt-16">
-        <h1 className="text-8xl flex flex-col gap-1 font-extralight">
+      <div className="z-10 text-white flex flex-col justify-center max-w-[1400px] w-[90vw] mx-auto pt-8 md:pt-16">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl flex flex-col gap-1 font-extralight">
           <span className="flex gap-2">Imagine</span>
           <HiArrowDown className="text-[#B76E79] my-2 text-4xl"/>
           <span className="flex gap-2">Plan</span>
