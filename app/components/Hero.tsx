84d5334403e14f2750b8c10fba26b3ce1f3f3119
Natasha Johnson <njohnson533@gmail.com>
2024-01-25 17:11:29 -0500

    chore: fix hero overflow

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,13 +6,13 @@ import { HiArrowDown } from 'react-icons/hi2';
 
 const Hero = () => {
   return (
-    <section className="flex h-[80vh] flex-col justify-center gap-12 relative">
+    <section className="flex h-[90vh] flex-col justify-center gap-12 relative">
       <Image src={hero} alt="Woman sitting on couch filling out weekly planner page." className="absolute top-0 left-0 w-full h-full object-cover" priority/>
 
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-80"></div>
       
       <div className="z-10 text-white flex flex-col justify-center max-w-[1400px] w-[90vw] mx-auto pt-8 md:pt-16">
-        <h1 className="text-6xl md:text-7xl lg:text-8xl xl:text-9xl flex flex-col gap-1 font-extralight">
+        <h1 className="text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl flex flex-col gap-1 font-extralight">
           <span className="flex gap-2">Imagine</span>
           <HiArrowDown className="text-[#B76E79] my-2 text-4xl"/>
           <span className="flex gap-2">Plan</span>
